7ddfd55a8221703e8bef98b62f0e4e1f1d677ffe
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2021-01-02 20:59:22 +0000

    ARM CPU Topology and cpu_power driver

--- g_model.py
+++ g_model.py
@@ -99,3 +99,59 @@ class seg_GAN(object):
 
 
         self.saver = tf.train.Saver(max_to_keep=50000)
+
+
+    def generator(self,input_op,batch_size_tf):
+               
+        ######## FCN for the 32x32x32 to 24x24x24 ###################################
+        conv1_1 = conv_op(input_op, name="g_conv1_1", kh=7, kw=7, n_out=32, dh=1, dw=1,wd=self.wd)#512x512
+        conv1_2 = conv_op(conv1_1, name="g_conv1_2", kh=7, kw=7, n_out=32, dh=1, dw=1,wd=self.wd)
+        pool1 = mpool_op(conv1_2,   name="g_pool1",   kh=2, kw=2, dw=2, dh=2)#256x256
+        conv2_1 = conv_op(pool1, name="g_conv2_1", kh=7, kw=7, n_out=64, dh=1, dw=1,wd=self.wd)#256x256
+        conv2_2 = conv_op(conv2_1, name="g_conv2_2", kh=7, kw=7, n_out=64, dh=1, dw=1,wd=self.wd)
+        pool2 = mpool_op(conv2_2,   name="g_pool2",   kh=2, kw=2, dw=2, dh=2)#128x128
+        conv3_1 = conv_op(pool2, name="g_conv3_1", kh=7, kw=7, n_out=96, dh=1, dw=1,wd=self.wd)
+        conv3_2 = conv_op(conv3_1, name="g_conv3_2", kh=7, kw=7, n_out=96, dh=1, dw=1,wd=self.wd)
+        pool3 = mpool_op(conv3_2,   name="g_pool2",   kh=2, kw=2, dw=2, dh=2)#64x64
+        conv4_1 = conv_op(pool3, name="g_conv4_1", kh=7, kw=7, n_out=128, dh=1, dw=1,wd=self.wd)
+        conv4_2 = conv_op(conv4_1, name="g_conv4_2", kh=7, kw=7, n_out=128, dh=1, dw=1,wd=self.wd)
+        deconv1 = deconv_op(conv4_2,    name="g_deconv1", kh=4, kw=4, n_out=64, wd=self.wd, batchsize=batch_size_tf)#128x128
+        concat1=concatenate_op(deconv1,conv3_2,name="g_concat1")
+        deconv2 = deconv_op(concat1,    name="g_deconv2", kh=4, kw=4, n_out=64, wd=self.wd, batchsize=batch_size_tf)#256x256
+        concat2=concatenate_op(deconv2,conv2_2,name="g_concat2")
+        deconv3 = deconv_op(concat2,    name="g_deconv3", kh=4, kw=4, n_out=32, wd=self.wd, batchsize=batch_size_tf)#512x512
+        concat3=concatenate_op(deconv3,conv1_2,name="g_concat3")
+        upscore = conv_op(concat3, name="g_upscore", kh=7, kw=7, n_out=self.num_classes, dh=1, dw=1,wd=self.wd,activation=False)#512x512
+        return upscore,upscore
+
+
+    def discriminator(self, inputCT, reuse=False):
+        if reuse:
+            tf.get_variable_scope().reuse_variables()
+        print 'ct shape ',inputCT.get_shape()
+        h0=conv_op_bn(inputCT, name="d_conv_dis_1_a", kh=5, kw=5, n_out=32, dh=1, dw=1, wd=self.wd, padding='VALID',train_phase=self.train_phase)
+        print 'h0 shape ',h0.get_shape()
+        m0=mpool_op(h0, 'pool0', kh=2, kw=2, dh=2, dw=2)
+        print 'm0 shape ',m0.get_shape()
+        h1 = conv_op_bn(m0, name="d_conv2_dis_a", kh=5, kw=5, n_out=64, dh=1, dw=1, wd=self.wd, padding='VALID',train_phase=self.train_phase)
+        print 'h1 shape ',h1.get_shape()
+        m1=mpool_op(h1, 'pool1', kh=2, kw=2, dh=2, dw=2)
+        print 'mi shape ',m1.get_shape()
+        h2 = conv_op_bn(m1, name="d_conv3_dis_a", kh=5, kw=5, n_out=128, dh=1, dw=1, wd=self.wd, padding='VALID',train_phase=self.train_phase)#28
+        h3 = conv_op_bn(h2, name="d_conv4_dis_a", kh=5, kw=5, n_out=64, dh=1, dw=1, wd=self.wd, padding='VALID',train_phase=self.train_phase)
+        fc1=fullyconnected_op(h3, name="d_fc1", n_out=64, wd=self.wd, activation=True)
+        fc2=fullyconnected_op(fc1, name="d_fc2", n_out=32, wd=self.wd, activation=True)
+        fc3=fullyconnected_op(fc2, name="d_fc3", n_out=1, wd=self.wd, activation=False)
+        return tf.nn.sigmoid(fc3), fc3
+
+
+
+
+    def train(self, config):
+        path_test=config.dir_patients#'/home/trullro/CT_cleaned/'
+        _, patients, _ = os.walk(path_test).next()#every folder is a patient
+        patients.sort()
+        patientstmp=patients[-4]
+        print 'global_step ', self.global_step.name
+        print 'lr_step ',self.lr_step
+        print 'trainable vars '
