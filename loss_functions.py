a85969809c6cbd28825c8d750c43179c1045efcd
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2021-09-04 19:41:22 +0000

    rettelser

--- loss_functions.py
+++ loss_functions.py
@@ -4,4 +4,6 @@ import numpy as np
 
 
 def loss_dice(logits, labels, num_classes,batch_size_tf):
-    """Calculate the loss from th
+    """Calculate the loss from the logits and the labels.
+    Args:
+      logits: tensor, float 
