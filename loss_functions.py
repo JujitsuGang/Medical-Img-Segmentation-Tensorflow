ea3b91200a1d537863330ac93e7308f3d1d9b805
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2021-10-02 18:03:01 +0000

    main file

--- loss_functions.py
+++ loss_functions.py
@@ -10,4 +10,6 @@ def loss_dice(logits, labels, num_classes,batch_size_tf):
           Use vgg_fcn.up as logits.
       labels: Labels tensor, int32 - [batch_size, width, height].
           The ground truth of your data.
-      weights: numpy array - [n
+      weights: numpy array - [num_classes]
+          Weighting the loss of each class
+  
