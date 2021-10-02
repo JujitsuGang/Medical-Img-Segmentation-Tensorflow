cbac6c065d1ae92f76d4efcc9c51fb6b2ff90371
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2021-10-02 11:20:00 +0000

    Update useLiveQuery().md

--- loss_functions.py
+++ loss_functions.py
@@ -9,4 +9,5 @@ def loss_dice(logits, labels, num_classes,batch_size_tf):
       logits: tensor, float - [batch_size, width, height, num_classes].
           Use vgg_fcn.up as logits.
       labels: Labels tensor, int32 - [batch_size, width, height].
-          The ground trut
+          The ground truth of your data.
+      weights: numpy array - [n
