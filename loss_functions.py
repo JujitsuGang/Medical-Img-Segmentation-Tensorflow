ce6f585506177424b1d20052e392f981adcbe5b3
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2021-11-21 22:33:12 +0000

    fixed a crash

--- loss_functions.py
+++ loss_functions.py
@@ -40,4 +40,5 @@ def lossfcn(logits, labels, num_classes,batch_size_tf,weights=None):
     Args:
       logits: tensor, float - [batch_size, width, height, num_classes].
           Use vgg_fcn.up as logits.
-      labels: Labels tensor, i
+      labels: Labels tensor, int32 - [batch_size, width, height, num_classes].
+          Th
