c385bd24bf105f8db59f4529474a9ef363e0bf6d
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2022-04-24 10:03:28 +0000

    Update the README file

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ import tensorflow as tf
 import pprint
 from g_model import seg_GAN
 os.environ['CUDA_VISIBLE_DEVICES'] = "2"
-pp = pprint.Pr
+pp = pprint.PrettyPrinter()
+
+flags = tf.app.flags
+flags.DEF
