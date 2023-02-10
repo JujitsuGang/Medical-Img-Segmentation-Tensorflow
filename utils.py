90b4382f45376dd41f848d6f09cf5dd5461692c9
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2023-02-10 08:57:45 +0000

    Refinement.

--- utils.py
+++ utils.py
@@ -28,3 +28,53 @@ def process_eso(vol_out):
     zmin=np.min(listorgan[0])
     zmax=np.max(listorgan[0])
     ini_found=False
+    for idx in xrange(zmin,zmax):
+        eso_slice=seg_eso[idx]
+        centroid=center_of_mass(eso_slice)
+        if not ini_found:#if we have not found the first slice empty
+            if np.isnan(centroid).any():#look for the first emppty slice
+                #print 'is NAN ',idx
+                ini=idx-1
+                pini=list(center_of_mass(seg_eso[idx-1]))
+                pini.append(idx-1)
+                ini_found=True
+        else:#if we have already found the first empty slice, look for the final one
+            idvox=np.where(eso_slice==1)
+            nvoxels=len(idvox[0])
+            if not np.isnan(centroid).any() and nvoxels>5:#the slice with data and enough voxels
+
+                #print 'final nan ',idx
+                fin=idx
+                pfin=list(center_of_mass(seg_eso[fin]))
+                pfin.append(idx)
+                #print 'pini ',pini
+                #print 'pfin ',pfin
+                for z in xrange(ini,fin):#we will fill the empty slices here
+                    newcenter=interpolateline(pini,pfin,z)
+                    #print 'new center ',newcenter
+                    #print 'prev center ',center_of_mass(seg_eso[z-1])
+                    translation=np.int16(np.array(newcenter)-np.array(center_of_mass(seg_eso[z-1])))
+                    #print 'trans ',translation
+                    #tx = tf.SimilarityTransform(translation=(0,0))#tuple(translation)
+                    if z==ini:
+                        slicetmp = shift(seg_eso[z-5],translation)#tf.warp(seg_eso[z-1], tx)
+                    else:
+                        slicetmp = shift(seg_eso[z-1],translation)#tf.warp(seg_eso[z-1], tx)
+                    #print 'unique slice befor trans ',np.unique(seg_eso[z-1])
+                    #print 'unique slice tmp ',np.unique(slicetmp)
+                    seg_eso[z]=slicetmp
+                ini_found=False
+    idxeso=np.where(seg_eso>0)
+    volfinal=np.copy(vol_out)
+    volfinal[idxesoini]=0
+    volfinal[idxeso]=1
+    return volfinal
+    
+    
+def interpolateline(p0,p1,z):
+    #p1 and p2 are 3d points x,y,z and z is the slice for which we want to compute x and y
+    
+    x=(float(z-p0[2])/(p1[2]-p0[2]))*(p1[0]-p0[0])+p0[0]
+    y=(float(z-p0[2])/(p1[2]-p0[2]))*(p1[1]-p0[1])+p0[1]
+    print 'x ',x
+    print 'y ',y
